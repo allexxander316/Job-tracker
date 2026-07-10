98c11822427460f8d0ae3de26d6fe549962858f1
alexander-smirnov <allexxander316@gmail.com>
2026-07-10 08:58:21 +0300

    feat: add vacancy sync service

---
+++ services.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+from db import get_by_external_id, insert_vacancy, update_vacancy
+from hh_client import get_vacancies
+
+
+def sync_all():
+    vacancies = get_vacancies()
+    for vacancy in vacancies:
+        vacancy_from_bd = get_by_external_id(external_id=vacancy["external_id"])
+
+        if vacancy_from_bd is None:
+            insert_vacancy(vacancy)
+            continue
+
+        comparable = {"header", "description", "url", "salary_from", "salary_to", "area", "experience"}
+
+        changed = any(
+            vacancy.get(field) != vacancy_from_bd.get(field)
+            for field in comparable
+        )
+
+        if changed:
+            vacancy["updated_at"] = datetime.now()
+            update_vacancy(vacancy)
